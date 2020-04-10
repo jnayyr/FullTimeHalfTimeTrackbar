2600a2b79f5b54d16dc777ea1a1cde0db751c2c7
Ajay Nair <AJ4942BO@go.minnstate.edu>
2020-04-10 16:07:57 -0500

    Add project files.

---
+++ FullTimeHalfTimeTrackbar/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace FullTimeHalfTimeTrackbar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trkCredits = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCredits = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trkCredits)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trkCredits
+            // 
+            this.trkCredits.Location = new System.Drawing.Point(63, 132);
+            this.trkCredits.Maximum = 24;
+            this.trkCredits.Name = "trkCredits";
+            this.trkCredits.Size = new System.Drawing.Size(492, 90);
+            this.trkCredits.TabIndex = 0;
+            this.trkCredits.Scroll += new System.EventHandler(this.trkCredits_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number of Credits";
+            // 
+            // lblCredits
+            // 
+            this.lblCredits.AutoSize = true;
+            this.lblCredits.Location = new System.Drawing.Point(601, 132);
+            this.lblCredits.Name = "lblCredits";
+            this.lblCredits.Size = new System.Drawing.Size(24, 25);
+            this.lblCredits.TabIndex = 2;
+            this.lblCredits.Text = "0";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(221, 225);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(190, 25);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Status shown here";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 378);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblCredits);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trkCredits);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trkCredits)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trkCredits;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCredits;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ FullTimeHalfTimeTrackbar/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FullTimeHalfTimeTrackbar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trkCredits_Scroll(object sender, EventArgs e)
+        {
+            int credits = trkCredits.Value;
+
+            // Show TrackBar value in lblCredits for user feedback
+            lblCredits.Text = trkCredits.Value.ToString();
+
+            // Calculate status and display in lblStatus
+            if (credits >= 12)
+            {
+                lblStatus.Text = "Full Time";
+            }
+            else if (credits >= 6)
+            {
+                lblStatus.Text = "Half Time";
+            }
+            else
+            {
+                lblStatus.Text = "Less than Half Time";
+            }
+        }
+    }
+}
